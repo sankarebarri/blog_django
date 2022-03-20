298f1364cc7fd84a446b7644554a2ae2fcdeb0ee
sankarebarri <sankarebarri_dev@yahoo.com>
2022-03-20 18:53:34 +0000

    home views coded

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import BlogDetails
 # Create your views here.
+def home(request):
+
+    context = {
+        'posts': BlogDetails.objects.all()
+    }
+
+    return render(request, 'home/home.html', context)
